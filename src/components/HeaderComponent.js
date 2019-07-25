88a14eef817d1352e1eab68c2a08158224197b43
pinedadaniel <danny.systemyes@gmail.com>
2019-07-24 23:52:05 -0500

    listo

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -9,25 +9,30 @@ class Header extends React.Component{
       
         <header class="header">
         <div class="container">
-            <h1>Mi Portafolio</h1>
-            
-            <nav class="menu">
-                <ul>
-                    <li>
-                        <a class="link" href="#">Portafolio</a>
-                    </li>
-                    <li>
-                        <a class="link" href="#eventos">Experiencia</a>
-                    </li>
-                    <li>
-                        <a class="link" href="#contacto">Trabajemos Juntos</a>
-                    </li>
-                </ul>
-            </nav>   
-
+        <nav class="navbar navbar-expand-lg navbar-light" >
+  <a class="navbar-brand" href="#">Mi Portafolio</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Portafolio <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Experiencias</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Trabajemos Juntos</a>
+      </li>
+   
+    </ul>
+  </div>
+</nav>
         </div>
     </header>
     )
+
 }
 }
 
--- src/components/ProfessionComponent.js
+++ src/components/ProfessionComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import iconImg from '../images/img.png'
 
 
 
@@ -6,26 +7,26 @@ import React from 'react';
 class Profession extends React.Component{
     render(){
     return  (
- <main>    
+  
 <section className='hero'> 
     <div className='container'>
     <div className="row">
-        <div className='col-xs-12 col-sm-3 col-md-8'>
-        <h1> Hola soy <strong> John Faber Florez </strong> <br></br> desarrollador
+        <div className='col-8 ' >
+        <h1 className='textProfession'> Hola soy <strong> John Faber Florez </strong> <br></br> desarrollador
       <strong> FrontEnd </strong> con <br></br> enfásis en <strong> JavaScript</strong> </h1>   
         </div>
 
-           <div className='col-xs-12 col-sm-9 col-md-4' >
-           <img className='hero-image' src='https://eafitrequisitos.s3.us-east-2.amazonaws.com/john.png' alt='imagen'/>    
+           <div className='col-4 ' id='contImg' >
+              
+           <img className='hero-image' id='img' src={iconImg}/>    
+            
             </div> 
-  
         </div>
     </div>
    
-     
-
+    
 </section>
-</main>    
+  
     );
 }
 }
